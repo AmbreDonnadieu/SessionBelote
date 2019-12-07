823189eb1bea0fb36aa58c4db5c34ed6a77bfebb
Forenceur <battaglia.eddy@gmail.com>
2019-12-07 21:40:22 +0100

    update file command + result in a different folder

--- MetriqueFiles/command.sh
+++ MetriqueFiles/command.sh
@@ -1,4 +1,5 @@
-cd ~/UQAC/8INF957/PMD
+cd ~/Documents/UQAC/8INF957/SessionBelote/MetriqueFiles
+mkdir -p ./resultat
 
 FILE1=programSizeInLinesOfCodes
 FILE2=documentation
@@ -8,4 +9,4 @@ METRICS=${FILE3}
 
 OUT_SUFFIXES=
 
-~/pmd-bin-6.18.0/bin/run.sh pmd -no-cache -t 2 -R ./xml/${METRICS}.xml -d ../JBelote/src/ -f text > ./${METRICS}${OUT_SUFFIXES}
+~/pmd-bin-6.18.0/bin/run.sh pmd -no-cache -t 2 -R ./xml/${METRICS}.xml -d ../JBelote/src/ -f text > ./resultat/${METRICS}${OUT_SUFFIXES}
